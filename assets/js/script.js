e86cfb2a7b6df54a49a78c169b1baad648638c63
AK <ak@mbp32ak.local>
2023-01-18 01:20:52 +0200

    Feature | Almost finished styling and responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,41 @@
-console.log("hello world :)))")
+const burgerButton = document.getElementById("burger_btn")
+const menu = document.getElementById("menu")
+const menuChildren = document.querySelectorAll("#menu a")
 
-const test = (val) => {
-    console.log(val)
-}
+burgerButton.addEventListener("click", () => {
+    if (menu.className === "burger-menu") {
+        document.body.style.overflow = "hidden"
+    } else {
+        document.body.style.overflow = ""
+    }
 
-test("Func invocation lol")
+    burgerButton.classList.toggle("active")
+    menu.classList.toggle("active")
+})
+
+menuChildren.forEach(item => {
+    item.addEventListener("click", () => {
+        if (menu.className === "burger-menu") {
+            document.body.style.overflow = "hidden"
+        } else {
+            document.body.style.overflow = ""
+        }
+        burgerButton.classList.remove("active")
+        menu.classList.remove("active")
+    })
+})
+
+const serviceBtn = document.querySelectorAll(".services__list-item")
+
+serviceBtn.forEach(item => {
+    item.addEventListener("click", () => {
+        const activeElem = document.querySelector(".services__list-item.active")
+        activeElem.classList.remove("active")
+        const activeElemAttr = activeElem.getAttribute("data-click")
+        document.querySelector(`[data-show=${activeElemAttr}]`).classList.remove("active")
+
+        const attr = item.getAttribute("data-click")
+        item.classList.add("active")
+        document.querySelector(`[data-show=${attr}]`).classList.add("active")
+    })
+})
